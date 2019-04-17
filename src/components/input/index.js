a256aa6b419d424054375c357e03254d47b0e3df
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2019-04-17 19:45:43 -0300

    feat() add input component

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -2,23 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = props => {
-    return (
-        <div className="form-group">
-            <label htmlFor={props.htmlFor}>{props.text}</label>
-            <input type={props.type} className="form-control" id={props.htmlFor} />
-            {props.htmlFor === 'email' ? (
-                <small id="emailHelp" className="form-text text-muted">
-                    Não iremos compartilhar seu email com ninguém.
-                </small>
-            ) : null}
-        </div>
-    );
+  return (
+    <div className="form-group">
+      <label htmlFor={props.htmlFor}>
+        {props.text}
+        {props.required ? '*' : null}
+      </label>
+      <input
+        type={props.type}
+        className="form-control"
+        id={props.htmlFor}
+        onChange={event => props.value(props.htmlFor, event.target.value)}
+        required={props.required}
+      />
+      {props.htmlFor === 'email' ? (
+        <small id="emailHelp" className="form-text text-muted">
+          Não iremos compartilhar seu email com ninguém.
+        </small>
+      ) : null}
+    </div>
+  );
 };
 
 Input.propTypes = {
-    text: PropTypes.string,
-    htmlFor: PropTypes.string,
-    id: PropTypes.string,
+  text: PropTypes.string,
+  htmlFor: PropTypes.string,
+  id: PropTypes.string,
 };
 
 export default Input;
